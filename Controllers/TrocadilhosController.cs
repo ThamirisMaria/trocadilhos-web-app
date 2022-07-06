167c1b8081efd26b717a0b33a483251ee5b05a66
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2022-07-05 21:08:39 -0300

    Add search form page

--- Controllers/TrocadilhosController.cs
+++ Controllers/TrocadilhosController.cs
@@ -25,6 +25,12 @@ namespace TrocadilhosWebApp.Controllers
             return View(await _context.Trocadilho.ToListAsync());
         }
 
+        // GET: Trocadilhos/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
         // GET: Trocadilhos/Details/5
         public async Task<IActionResult> Details(int? id)
         {
